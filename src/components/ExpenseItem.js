d62b8a1f8a88704951dec9798fd1989e3d32b5e4
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-02 18:45:37 -0300

    dummy data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 2, 28)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67
+
   return (
     <div className='expense-item'>
-      <div>March 28th, 2021</div>
+      <div>{ expenseDate.toISOString() }</div>
       <div className='expense-item__description'>
-        <h2>Car insurance</h2>
-        <div className='expense-item__price'>R$294.67</div>
+        <h2>{ expenseTitle }</h2>
+        <div className='expense-item__price'>${ expenseAmount }</div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ExpenseItem;
+export default ExpenseItem
